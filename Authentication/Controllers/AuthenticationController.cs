652464dbf7185cb50f5ca354aef8a6c2986c68c5
arleyribeiro <arley.sribeiro@gmail.com>
2021-04-30 15:12:42 -0300

    Remove endpoints

--- Authentication/Controllers/AuthenticationController.cs
+++ Authentication/Controllers/AuthenticationController.cs
@@ -45,26 +45,5 @@ namespace Authentication.Controllers
             var result = await _accountService.Register(_user).ConfigureAwait(false);
             return result;
         }
-
-        [HttpGet]
-        [Route("anonymous")]
-        [AllowAnonymous]
-        public string Anonymous() => "Anônimo";
-
-        [HttpGet]
-        [Route("authenticated")]
-        [Authorize]
-        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
-
-        [HttpGet]
-        [Route("employee")]
-        [Authorize(Roles = "employee,manager")]
-        public string Employee() => "Funcionário";
-
-        [HttpGet]
-        [Route("manager")]
-        [Authorize(Roles = "manager")]
-        public string Manager() => "Gerente";
-
     }
 }
